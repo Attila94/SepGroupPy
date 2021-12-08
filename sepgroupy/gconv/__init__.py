bb42c7b7c9b325ff77cd612ab9261b79377ce052
Attila Lengyel <a.lengyel@tudelft.nl>
2021-12-08 11:49:21 +0100

    bugfix

--- sepgroupy/gconv/__init__.py
+++ sepgroupy/gconv/__init__.py
@@ -1,3 +1,3 @@
-from groupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
-from groupy.gconv.pytorch_gconv.g_splitgconv2d import gP4ConvZ2, gP4ConvP4, gP4MConvZ2, gP4MConvP4M
-from groupy.gconv.pytorch_gconv.gc_splitgconv2d import gcP4ConvZ2, gcP4ConvP4, gcP4MConvZ2, gcP4MConvP4M
+from sepgroupy.gconv.pytorch_gconv.splitgconv2d import P4ConvZ2, P4ConvP4, P4MConvZ2, P4MConvP4M
+from sepgroupy.gconv.pytorch_gconv.g_splitgconv2d import gP4ConvZ2, gP4ConvP4, gP4MConvZ2, gP4MConvP4M
+from sepgroupy.gconv.pytorch_gconv.gc_splitgconv2d import gcP4ConvZ2, gcP4ConvP4, gcP4MConvZ2, gcP4MConvP4M
